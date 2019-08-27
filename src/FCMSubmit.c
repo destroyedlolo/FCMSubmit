46c1ce8e9cf8220e4d19a4a861f62202085beeba
destroyedlolo <l_faillie@yahoo.com>
2019-08-28 01:01:01 +0200

    Read configuration

--- src/FCMSubmit.c
+++ src/FCMSubmit.c
@@ -21,19 +21,96 @@
 #include <stdlib.h>		/* exit(), ... */
 #include <unistd.h>		/* getopt(), ... */
 #include <libgen.h>		/* basename(), ... */
+#include <assert.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 
 	/***
 	 * Default configuration
 	 ***/
+
 #define DEFAULT_CONFIGURATION_FILE	"/usr/local/etc/FCMSubmit.conf"
 #define VERSION "0.1"
+
+
+	/***
+	 * Configuration
+	 ***/
+
+#define MAXLINE 1024
+
+bool verbose = false;
+const char *token = NULL;
+const char *senderID = NULL;
+
+
+	/***
+	 * Helpers
+	 ***/
+
+char *removeLF(char *s){
+	size_t l=strlen(s);
+	if(l && s[--l] == '\n')
+		s[l] = 0;
+	return s;
+}
+
+char *striKWcmp( char *s, const char *kw ){
+/* compare string s against kw
+ * Return :
+ * 	- remaining string if the keyword matches
+ * 	- NULL if the keyword is not found
+ */
+	size_t klen = strlen(kw);
+	if( strncasecmp(s,kw,klen) )
+		return NULL;
+	else
+		return s+klen;
+}
+
+	/***
+	 * Let's go ...
+	 ***/
+
+void read_configuration( const char *fch){
+	char l[MAXLINE];
+	FILE *f;
+	char *arg;
+
+	if(verbose)
+		printf("\nReading configuration file '%s'\n---------------------------\n", fch);
+
+	if(!(f=fopen(fch, "r"))){
+		perror( fch );
+		exit(EXIT_FAILURE);
+	}
+
+	while(fgets(l, MAXLINE, f)){
+		if(*l == '#' || *l == '\n')
+			continue;
+
+		if((arg = striKWcmp(l,"token="))){
+			assert( token = strdup( removeLF(arg) ) );
+			if(verbose)
+				printf("API token : '%s'\n", token);
+		} else if((arg = striKWcmp(l,"senderID="))){
+			assert( senderID = strdup( removeLF(arg) ) );
+			if(verbose)
+				printf("senderID : '%s'\n", senderID);		}
+	}
+
+	if( !token || !senderID ){
+		fputs("Missing a mandatory parameter in the configuration file\n", stderr);
+		exit(EXIT_FAILURE);
+	}
+}
 
 int main( int ac, char ** av){
 	const char *conf_file = DEFAULT_CONFIGURATION_FILE;
 
 	int c;
-	while((c = getopt(ac, av, "vhf:t")) != EOF) switch(c){
+	while((c = getopt(ac, av, "vhf:")) != EOF) switch(c){
 	case 'h':
 		fprintf(stderr, "%s (%s)\n"
 			"Submit Firebase Cloud Messaging notification\n"
@@ -45,7 +122,18 @@ int main( int ac, char ** av){
 			basename(av[0]), VERSION, DEFAULT_CONFIGURATION_FILE
 		);
 		exit(EXIT_FAILURE);
-		break;	
+		break;
+	case 'v':
+		verbose = true;
+		break;
+	case 'f':
+		conf_file = optarg;
+		break;
+	default:
+		fprintf(stderr, "Unknown option\n%s -h\n\tfor some help\n", av[0] );
+		exit(EXIT_FAILURE);
 	}
+	read_configuration( conf_file );
 
+	exit(EXIT_SUCCESS);
 }
